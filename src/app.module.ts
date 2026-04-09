17b762daebb9dd6c06a104da4a2816fa4402943d
kevinkeyssx <keyss1700@gmail.com>
2026-04-08 21:33:35 -0400

    Modules implemented

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
+import { AppController }    from '@app/app.controller';
+import { PrismaModule }     from '@prisma/prisma.module';
+import { StaffModule }      from '@staff/staff.module';
+import { ImagesModule }     from '@images/images.module';
+import { TemplatesModule }  from '@templates/templates.module';
+import { StudentsModule }   from '@students/students.module';
+import { SendEmailsModule } from '@send-emails/send-emails.module';
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports     : [ 
+        PrismaModule, 
+        StaffModule, 
+        ImagesModule, 
+        TemplatesModule, 
+        StudentsModule, 
+        SendEmailsModule 
+    ],
+    controllers : [ AppController ],
 })
 export class AppModule {}
