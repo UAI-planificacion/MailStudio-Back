7c336286c46e248fe1e030377b689f01ab5ecc29
kevinkeyssx <keyss1700@gmail.com>
2026-04-08 21:34:29 -0400

    Pipes & prefix & Swagger configured

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,40 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {
+    DocumentBuilder,
+    SwaggerModule
+}                                   from '@nestjs/swagger';
+import { Logger, ValidationPipe }   from '@nestjs/common';
+import { NestFactory }              from '@nestjs/core';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
-}
-bootstrap();
+import { AppModule }    from '@app/app.module';
+import { ENVS }         from '@config/envs';
+
+
+( async () => {
+    const logger    = new Logger( 'Main' );
+    const app       = await NestFactory.create( AppModule );
+
+    app
+    .setGlobalPrefix( 'api/v1' )
+    .useGlobalPipes( new ValidationPipe({
+        whitelist: true,
+        transform: true
+    }))
+    .enableCors({
+        origin      : ENVS.ALLOWED_ORIGINS,
+        methods     : ['GET', 'POST', 'DELETE', 'OPTIONS'],
+        credentials : true,
+    });
+
+    const config = new DocumentBuilder()
+        .setTitle( 'MailStudio API' )
+        .setDescription( 'The MailStudio API documentation' )
+        .setVersion( '1.0' )
+        .build();
+
+    const documentFactory = SwaggerModule.createDocument( app, config );
+
+    SwaggerModule.setup( 'api/docs', app, documentFactory );
+
+    await app.listen( ENVS.PORT );
+    logger.log( `MailStudio listening on port ${ ENVS.PORT }` );
+})();
