48f1b74df5428ef234500a4ab4bada2577677bd4
kevinkeyssx <keyss1700@gmail.com>
2026-04-09 23:06:27 -0400

    try catch & PrismaException implemented

--- src/staff/staff.service.ts
+++ src/staff/staff.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 
 import { PrismaService }    from '@prisma/prisma.service';
+import { PrismaException }  from '@prisma/prisma-catch';
 import { CreateStaffDto }   from '@staff/dto/create-staff.dto';
 import { UpdateStaffDto }   from '@staff/dto/update-staff.dto';
 
@@ -12,9 +13,13 @@ export class StaffService {
 
 
 	async create( createStaffDto : CreateStaffDto ) {
-		return await this.prisma.staff.create({
-			data	: createStaffDto,
-		});
+        try {
+            return await this.prisma.staff.create({
+                data	: createStaffDto,
+            });
+        } catch ( error ) {
+            throw PrismaException.catch( error );
+        }
 	}
 
 
@@ -23,13 +28,13 @@ export class StaffService {
 	}
 
 
-	async findOne( id : string ) {
+	async findOneByEmail( email : string ) {
 		const staff = await this.prisma.staff.findUnique({
-			where : { id },
+			where : { email },
 		});
 
 		if ( !staff ) {
-			throw new NotFoundException ( `Staff with id ${ id } not found` );
+			throw new NotFoundException ( `Staff with email ${ email } not found` );
 		}
 
 		return staff;
@@ -37,21 +42,25 @@ export class StaffService {
 
 
 	async update( id : string, updateStaffDto : UpdateStaffDto ) {
-		await this.findOne( id );
-
-		return await this.prisma.staff.update({
-			where	: { id },
-			data	: updateStaffDto,
-		});
+        try {
+            return await this.prisma.staff.update({
+                where	: { id },
+                data	: updateStaffDto,
+            });
+        } catch ( error ) {
+            throw PrismaException.catch( error );
+        }
 	}
 
 
 	async remove( id : string ) {
-		await this.findOne( id );
-
-		return await this.prisma.staff.delete({
-			where : { id },
-		});
+        try {
+            return await this.prisma.staff.delete({
+                where : { id },
+            });
+        } catch ( error ) {
+            throw PrismaException.catch( error );
+        }
 	}
 
 }
