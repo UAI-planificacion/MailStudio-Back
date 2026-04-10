a2796f0b8487e9c956f06e512f1c7f3e6bb90eb2
kevinkeyssx <keyss1700@gmail.com>
2026-04-09 23:06:01 -0400

    findOneByEmail renamed

--- src/staff/staff.controller.ts
+++ src/staff/staff.controller.ts
@@ -1,35 +1,51 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { StaffService } from './staff.service';
-import { CreateStaffDto } from './dto/create-staff.dto';
-import { UpdateStaffDto } from './dto/update-staff.dto';
+
+import { StaffService }     from '@staff/staff.service';
+import { CreateStaffDto }   from '@staff/dto/create-staff.dto';
+import { UpdateStaffDto }   from '@staff/dto/update-staff.dto';
+
 
 @Controller( 'staff' )
 export class StaffController {
 
 	constructor( private readonly staffService: StaffService ) {}
 
+
 	@Post()
-	create( @Body() createStaffDto: CreateStaffDto ) {
+	create(
+        @Body() createStaffDto: CreateStaffDto
+    ) {
 		return this.staffService.create( createStaffDto );
 	}
 
-	@Get()
+
+    @Get()
 	findAll() {
 		return this.staffService.findAll();
 	}
 
-	@Get( ':id' )
-	findOne( @Param( 'id' ) id: string ) {
-		return this.staffService.findOne( id );
+
+    @Get( ':email' )
+	findOneByEmail(
+        @Param( 'email' ) email: string
+    ) {
+		return this.staffService.findOneByEmail( email );
 	}
 
-	@Patch( ':id' )
-	update( @Param( 'id' ) id: string, @Body() updateStaffDto: UpdateStaffDto ) {
+
+    @Patch( ':id' )
+	update(
+        @Param( 'id' ) id: string,
+        @Body() updateStaffDto: UpdateStaffDto
+    ) {
 		return this.staffService.update( id, updateStaffDto );
 	}
 
-	@Delete( ':id' )
-	remove( @Param( 'id' ) id: string ) {
+
+    @Delete( ':id' )
+	remove(
+        @Param( 'id' ) id: string
+    ) {
 		return this.staffService.remove( id );
 	}
 
