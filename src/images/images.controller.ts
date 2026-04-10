81d6d701f633c63362d4d85a3283bc121aaa53e7
kevinkeyssx <keyss1700@gmail.com>
2026-04-09 23:03:59 -0400

    ImageParamsDto implemented

--- src/images/images.controller.ts
+++ src/images/images.controller.ts
@@ -9,26 +9,37 @@ import {
     UseInterceptors, 
     UploadedFile 
 }                           from '@nestjs/common';
+import { 
+    ApiBody, 
+    ApiConsumes, 
+    ApiParam, 
+    ApiTags 
+}                           from '@nestjs/swagger';
 import { FileInterceptor }  from '@nestjs/platform-express';
 
 import { ImagesService }    from '@images/images.service';
 import { CreateImageDto }   from '@images/dto/create-image.dto';
 import { UpdateImageDto }   from '@images/dto/update-image.dto';
+import { ImageParamsDto }   from '@images/dto/image-params.dto';
 
 
+@ApiTags( 'Images' )
 @Controller( 'images' )
 export class ImagesController {
 
-    constructor( private readonly imagesService : ImagesService ) {}
+    constructor( private readonly imagesService : ImagesService ) { }
 
 
-    @Post()
+    @Post( ':nameImg' )
+    @ApiConsumes( 'multipart/form-data' )
+    @ApiParam({ name : 'nameImg', type : 'string', description : 'Nombre de la imagen' })
+    @ApiBody({ type : CreateImageDto })
     @UseInterceptors( FileInterceptor( 'file' ))
-    create (
-        @Body() createImageDto : CreateImageDto,
-        @UploadedFile() file   : Express.Multer.File,
+    upload(
+        @Param( ) params        : ImageParamsDto,
+        @UploadedFile( ) file   : Express.Multer.File,
     ) {
-        return this.imagesService.create( createImageDto, file );
+        return this.imagesService.create( params.nameImg, file );
     }
 
 
