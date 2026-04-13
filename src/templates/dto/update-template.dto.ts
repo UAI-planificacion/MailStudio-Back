0025e30da87292b0243e764092a4d03f52664409
kevinkeyssx <keyss1700@gmail.com>
2026-04-13 19:17:38 -0400

    UpdateTemplateDto updated active field implemented

--- src/templates/dto/update-template.dto.ts
+++ src/templates/dto/update-template.dto.ts
@@ -1,4 +1,13 @@
 import { PartialType } from '@nestjs/mapped-types';
-import { CreateTemplateDto } from './create-template.dto';
 
-export class UpdateTemplateDto extends PartialType(CreateTemplateDto) {}
+import { CreateTemplateDto }        from './create-template.dto';
+import { IsBoolean, IsOptional }    from 'class-validator';
+
+
+export class UpdateTemplateDto extends PartialType( CreateTemplateDto ) {
+
+    @IsBoolean()
+    @IsOptional()
+    active?: boolean;
+
+}
