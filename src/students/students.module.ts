505b55df5712c60c5da6a1d4b9f1eddd2096765d
kevinkeyssx <keyss1700@gmail.com>
2026-04-17 01:14:30 -0400

    Student module updated

--- src/students/students.module.ts
+++ src/students/students.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
-import { StudentsService } from './students.service';
-import { StudentsController } from './students.controller';
+
+import { PrismaModule }         from '@prisma/prisma.module';
+import { StudentsController }   from '@students/students.controller';
+import { StudentsService }      from '@students/students.service';
+
 
 @Module({
-  controllers: [StudentsController],
-  providers: [StudentsService],
+	imports     : [ PrismaModule ],
+	controllers : [ StudentsController ],
+	providers   : [ StudentsService ],
 })
 export class StudentsModule {}
