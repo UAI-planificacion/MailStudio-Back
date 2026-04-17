3a7a16e435dca7ad63a1717609d426a564a75ea5
kevinkeyssx <keyss1700@gmail.com>
2026-04-17 01:14:17 -0400

    search controller implemented

--- src/students/students.controller.ts
+++ src/students/students.controller.ts
@@ -1,34 +1,54 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { StudentsService } from './students.service';
-import { CreateStudentDto } from './dto/create-student.dto';
-import { UpdateStudentDto } from './dto/update-student.dto';
+import {
+    Controller,
+    Get,
+    Post,
+    Body,
+    Patch,
+    Param,
+    Delete,
+    HttpCode
+} from '@nestjs/common';
 
-@Controller('students')
+import { StudentsService }  from '@students/students.service';
+import { SearchStudentDto } from '@students/dto/search-student.dto';
+
+
+@Controller( 'students' )
 export class StudentsController {
-  constructor(private readonly studentsService: StudentsService) {}
+	constructor(
+        private readonly studentsService : StudentsService
+    ) {}
 
-  @Post()
-  create(@Body() createStudentDto: CreateStudentDto) {
-    return this.studentsService.create(createStudentDto);
-  }
+	@Post( 'search' )
+	@HttpCode( 200 )
+	search(
+        @Body() filters : SearchStudentDto
+    ) {
+		return this.studentsService.findStudents( filters );
+	}
 
-  @Get()
-  findAll() {
-    return this.studentsService.findAll();
-  }
+	// @Post()
+    // create(@Body() createStudentDto: CreateStudentDto) {
+    //     return this.studentsService.create(createStudentDto);
+    // }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.studentsService.findOne(+id);
-  }
+    // @Get()
+    // findAll() {
+    //     return this.studentsService.findAll();
+    // }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateStudentDto: UpdateStudentDto) {
-    return this.studentsService.update(+id, updateStudentDto);
-  }
+    // @Get(':id')
+    // findOne(@Param('id') id: string) {
+    //     return this.studentsService.findOne(+id);
+    // }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.studentsService.remove(+id);
-  }
+    // @Patch(':id')
+    // update(@Param('id') id: string, @Body() updateStudentDto: UpdateStudentDto) {
+    //     return this.studentsService.update(+id, updateStudentDto);
+    // }
+
+    // @Delete(':id')
+    // remove(@Param('id') id: string) {
+    //     return this.studentsService.remove(+id);
+    // }
 }
