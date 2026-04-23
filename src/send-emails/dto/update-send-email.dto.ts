c5187060657439c719d0be3293437926b43ec064
kevinkeyssx <keyss1700@gmail.com>
2026-04-22 22:18:26 -0400

    dtos deleted

--- src/send-emails/dto/update-send-email.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateSendEmailDto } from './create-send-email.dto';
-
-export class UpdateSendEmailDto extends PartialType(CreateSendEmailDto) {}
