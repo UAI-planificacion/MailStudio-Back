129a2bf49c11db14d34bf9a5dc0419dce3e536cb
kevinkeyssx <keyss1700@gmail.com>
2026-04-22 22:18:34 -0400

    SendEmailDto added

---
+++ src/send-emails/dto/send-email.dto.ts
@@ -0,0 +1,37 @@
+import {
+    ArrayMaxSize,
+    ArrayMinSize,
+    IsArray,
+    IsEmail,
+    IsNotEmpty,
+    IsString,
+    Length
+} from "class-validator";
+
+
+export class SendEmailDto {
+
+    @IsArray( { message: 'El campo emails debe ser un array' } )
+    @IsString( { each: true, message: 'Todos los elementos de emails deben ser strings' } )
+    @IsNotEmpty( { message: 'El campo emails no puede estar vacío' } )
+    @ArrayMinSize( 1, { message: 'El campo emails debe tener al menos 1 correo' } )
+    @ArrayMaxSize( 500000, { message: 'El campo emails puede tener un máximo de 500,000 correos' } )
+    @IsEmail( {}, { each: true, message: 'Todos los correos deben tener un formato válido' } )
+    @Length( 5, 200, { each: true, message: 'Cada correo debe tener entre 5 y 200 caracteres' } )
+    emails: string[];
+
+    @IsString( { message: 'El campo template debe ser un string' } )
+    @IsNotEmpty( { message: 'El campo template no puede estar vacío' } )
+    @Length( 1, 10000, { message: 'El campo template debe tener al menos 1 caracter y maximo 10000' } )
+    template: string;
+
+}
+
+
+
+export class SendEmailScheduleDto extends SendEmailDto {
+    @IsString( { message: 'El campo sendAt debe ser un string' } )
+    @IsNotEmpty( { message: 'El campo sendAt no puede estar vacío' } )
+    @Length( 1, 100, { message: 'El campo sendAt debe tener al menos 1 caracter y maximo 100' } )
+    sendAt: string;
+}
