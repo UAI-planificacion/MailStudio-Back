43bd770ded516f70458ef7a3a075114159359c60
kevinkeyssx <keyss1700@gmail.com>
2026-04-24 19:41:00 -0400

    sendMassive updated & sendWorkflow implemented

--- src/send-emails/send-emails.controller.ts
+++ src/send-emails/send-emails.controller.ts
@@ -6,6 +6,7 @@ import {
 }                               from '@send-emails/dto/send-email.dto';
 import { SendEmailsService }    from '@send-emails/send-emails.service';
 import { PayloadEmail }         from '@send-emails/models/payloadEmail.model';
+import { SendEmailWorkflowDto } from './dto/send-emal-workflow.dto';
 
 
 @Controller( 'mail-sender' )
@@ -19,44 +20,41 @@ export class SendEmailsController {
     async sendMassive(
         @Body() payload: SendEmailDto
     ) {
-        const messages = payload.emails.map(email => ({
-            email       : email,
-            template    : payload.template,
-            subject     : payload.subject
-        }));
-
-        await this.sendEmailsService.sendMassiveEmails( messages );
-
-        return {
-            status  : 'Encolado exitosamente',
-            count   : messages.length
-        };
+        return await this.sendEmailsService.startEmailJob( payload );
     }
 
 
-    @Post('schedule')
-    async scheduleCampaign(
-        @Body() payload: SendEmailScheduleDto
+    @Post( 'workflow' )
+    async sendWorkflow(
+        @Body() payload: SendEmailWorkflowDto
     ) {
-        const scheduledDate = new Date( payload.sendAt );
-
-        if ( scheduledDate <= new Date() ) {
-            throw new BadRequestException( 'La fecha de programación debe ser futura.' );
-        }
-
-        const messages: PayloadEmail[] = payload.emails.map( email => ({
-            email       : email,
-            template    : payload.template,
-            subject     : payload.subject
-        }));
-
-        await this.sendEmailsService.sendScheduledEmails( messages, scheduledDate );
-
-        return {
-            status          : 'Campaña programada exitosamente',
-            totalEmails     : messages.length,
-            scheduledFor    : scheduledDate.toISOString()
-        };
+        return await this.sendEmailsService.startEmailWorkflow( payload );
     }
+
+
+    // @Post('schedule')
+    // async scheduleCampaign(
+    //     @Body() payload: SendEmailScheduleDto
+    // ) {
+    //     const scheduledDate = new Date( payload.sendAt );
+
+    //     if ( scheduledDate <= new Date() ) {
+    //         throw new BadRequestException( 'La fecha de programación debe ser futura.' );
+    //     }
+
+    //     const messages: PayloadEmail[] = payload.students.map( student => ({
+    //         student,
+    //         templateId  : payload.templateId,
+    //         subject     : payload.subject
+    //     }));
+
+    //     await this.sendEmailsService.sendScheduledEmails( messages, scheduledDate );
+
+    //     return {
+    //         status          : 'Campaña programada exitosamente',
+    //         totalEmails     : messages.length,
+    //         scheduledFor    : scheduledDate.toISOString()
+    //     };
+    // }
 
 }
