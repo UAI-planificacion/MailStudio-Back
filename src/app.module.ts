1123a5eeffca56ee49073de39f7a8f60daa75de5
kevinkeyssx <keyss1700@gmail.com>
2026-04-24 19:40:08 -0400

    WorkflowModule implemented

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { ImagesModule }     from '@images/images.module';
 import { TemplatesModule }  from '@templates/templates.module';
 import { StudentsModule }   from '@students/students.module';
 import { SendEmailsModule } from '@send-emails/send-emails.module';
+import { WorkflowModule }   from './workflow/workflow.module';
 import { SeedModule }       from '@seed/seed.module';
 import { DataModule }       from '@data/data.module';
 
@@ -20,7 +21,8 @@ import { DataModule }       from '@data/data.module';
         StudentsModule, 
         SendEmailsModule,
         SeedModule,
-        DataModule 
+        DataModule,
+        WorkflowModule 
     ],
     controllers : [ AppController ],
 })
