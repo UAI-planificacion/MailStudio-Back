2cb908ab84e6b229a0367cd2ee9d97bd8a133182
kevinkeyssx <keyss1700@gmail.com>
2026-04-28 23:24:01 -0400

    AppModule updated  & SendEmailLogsModule implemented

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,28 +1,30 @@
 import { Module } from '@nestjs/common';
 
-import { AppController }    from '@app/app.controller';
-import { PrismaModule }     from '@prisma/prisma.module';
-import { StaffModule }      from '@staff/staff.module';
-import { ImagesModule }     from '@images/images.module';
-import { TemplatesModule }  from '@templates/templates.module';
-import { StudentsModule }   from '@students/students.module';
-import { SendEmailsModule } from '@send-emails/send-emails.module';
-import { WorkflowModule }   from './workflow/workflow.module';
-import { SeedModule }       from '@seed/seed.module';
-import { DataModule }       from '@data/data.module';
+import { AppController }        from '@app/app.controller';
+import { PrismaModule }         from '@prisma/prisma.module';
+import { StaffModule }          from '@staff/staff.module';
+import { ImagesModule }         from '@images/images.module';
+import { TemplatesModule }      from '@templates/templates.module';
+import { StudentsModule }       from '@students/students.module';
+import { SendEmailsModule }     from '@send-emails/send-emails.module';
+import { SendEmailLogsModule }  from '@send-email-logs/send-email-logs.module';
+import { WorkflowModule }       from '@workflow/workflow.module';
+import { SeedModule }           from '@seed/seed.module';
+import { DataModule }           from '@data/data.module';
 
 
 @Module({
-    imports     : [ 
-        PrismaModule, 
-        StaffModule, 
-        ImagesModule, 
-        TemplatesModule, 
-        StudentsModule, 
+    imports     : [
+        PrismaModule,
+        StaffModule,
+        ImagesModule,
+        TemplatesModule,
+        StudentsModule,
         SendEmailsModule,
         SeedModule,
         DataModule,
-        WorkflowModule 
+        WorkflowModule,
+        SendEmailLogsModule
     ],
     controllers : [ AppController ],
 })
