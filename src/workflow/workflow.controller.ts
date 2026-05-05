5509c89924a168ce3fbe295e51fe2eef0b8bf536
kevinkeyssx <keyss1700@gmail.com>
2026-05-05 19:53:41 -0400

    create & update & delete workflow controllers implemented

--- src/workflow/workflow.controller.ts
+++ src/workflow/workflow.controller.ts
@@ -1,7 +1,21 @@
-import { Controller, Get, Post, Param } from '@nestjs/common';
-import { WorkflowService } from './workflow.service';
+import {
+    Body,
+    Controller,
+    Delete,
+    Get,
+    Param,
+    Patch,
+    Post,
+    Query
+} from '@nestjs/common';
 
-@Controller('workflow')
+import { WorkflowService }      from '@workflow/workflow.service';
+import { UpdateWorkflowDto }    from '@workflow/dto/update-workflow.dto';
+import { SendEmailWorkflowDto } from '@send-emails/dto/send-email-workflow.dto';
+import { FindAllWorkflowDto }   from './dto/find-all-workflow.dto';
+
+
+@Controller( 'workflow' )
 export class WorkflowController {
 
     constructor(
@@ -10,14 +24,35 @@ export class WorkflowController {
 
 
     @Get()
-    async getAll() {
-        return await this.workflowService.findAll();
+    async getAll( @Query() query: FindAllWorkflowDto ) {
+        return await this.workflowService.findAll( query );
     }
 
 
     @Get( ':id' )
     async getOne( @Param( 'id' ) id: string ) {
         return await this.workflowService.findOne( id );
+    }
+
+
+    @Post()
+    async create( @Body() createWorkflowDto: SendEmailWorkflowDto ) {
+        return await this.workflowService.create( createWorkflowDto );
+    }
+
+
+    @Patch( ':id' )
+    async update(
+        @Param( 'id' ) id: string,
+        @Body() updateWorkflowDto: UpdateWorkflowDto
+    ) {
+        return await this.workflowService.update( id, updateWorkflowDto );
+    }
+
+
+    @Delete( ':id' )
+    async remove( @Param( 'id' ) id: string ) {
+        return await this.workflowService.remove( id );
     }
 
 
