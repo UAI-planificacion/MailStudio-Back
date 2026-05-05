430223053cf2dcf61f8a4f4d24db200a4f7c1380
kevinkeyssx <keyss1700@gmail.com>
2026-05-05 19:53:55 -0400

    WorkflowValidationService in module implemented

--- src/workflow/workflow.module.ts
+++ src/workflow/workflow.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 
-import { WorkflowService }      from './workflow.service';
-import { WorkflowController }   from './workflow.controller';
+import { WorkflowService }              from '@workflow/workflow.service';
+import { WorkflowController }           from '@workflow/workflow.controller';
+import { WorkflowValidationService }    from '@common/service/workflow';
+
 
 @Module({
     controllers : [ WorkflowController ],
-    providers   : [ WorkflowService ],
+    providers   : [ WorkflowService, WorkflowValidationService ],
 })
 export class WorkflowModule {}
