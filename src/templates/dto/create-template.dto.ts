7395af4c26635797ec6e2d1e8e142114c3c7eae6
kevinkeyssx <keyss1700@gmail.com>
2026-05-07 21:05:07 -0400

    images optional implemented

--- src/templates/dto/create-template.dto.ts
+++ src/templates/dto/create-template.dto.ts
@@ -31,11 +31,6 @@ export class CreateTemplateDto {
     @Length( 26, 26, { message: 'El campo updatedBy debe tener un máximo de 26 caracteres' } )
 	updatedBy?: string;
 
-    @IsString({ message: 'El campo subject debe ser un string' })
-	@IsOptional()
-    @MaxLength( 255, { message: 'El campo subject debe tener un máximo de 255 caracteres' } )
-	subject?: string;
-
     @IsOptional()
     @IsString( { each: true, message: 'Todos los elementos de cc deben ser strings' } )
     @ArrayMaxSize( 10, { message: 'El campo cc puede tener un máximo de 10 destinatarios' } )
@@ -47,5 +42,11 @@ export class CreateTemplateDto {
     @ArrayMaxSize( 10, { message: 'El campo bcc puede tener un máximo de 10 destinatarios' } )
     @IsEmail( {}, { each: true, message: 'Todos los correos deben tener un formato válido' } )
     bcc?: string[];
+
+    @IsOptional()
+    @IsString( { each: true, message: 'Todos los elementos de images deben ser strings' } )
+    @Length( 26, 26, { each: true, message: 'Cada elemento de images debe ser un ID de imagen válido (26 caracteres)' } )
+    @ArrayMaxSize( 20, { message: 'El campo images puede tener un máximo de 20 elementos' } )
+    images?: string[];
 
 }
