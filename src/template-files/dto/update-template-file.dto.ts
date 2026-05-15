e37178b6c9b7a8e82ab9e27056383f96aaee35bd
kevinkeyssx <keyss1700@gmail.com>
2026-05-14 21:47:35 -0400

    UpdateTemplateFileDto updated

--- src/template-files/dto/update-template-file.dto.ts
+++ src/template-files/dto/update-template-file.dto.ts
@@ -1,16 +1,32 @@
-import { PartialType } from '@nestjs/swagger';
-import { 
-    IsNotEmpty, 
-    IsString 
-}                      from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
-import { CreateTemplateFileDto } from './create-template-file.dto';
+import {
+    IsNotEmpty,
+    IsString,
+    Length
+} from 'class-validator';
 
 
-export class UpdateTemplateFileDto extends PartialType( CreateTemplateFileDto ) {
+export class UpdateTemplateFileDto {
 
+    @ApiProperty({
+        description : 'ID del creador',
+        example     : '01F8MECHZX3TBDSZ7XRADM79XE',
+    })
     @IsString()
     @IsNotEmpty()
+    @Length( 26, 26 )
     updatedBy : string;
+
+    @ApiProperty({
+        description : 'Nombre del archivo',
+        example     : 'mi-archivo',
+        minLength   : 1,
+        maxLength   : 100,
+    })
+    @IsString()
+    @IsNotEmpty()
+    @Length( 1, 100 )
+    name : string;
 
 }
