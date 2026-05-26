01b046ddaa84fad9b9a4f88809191cf5a73b76a3
kevinkeyssx <keyss1700@gmail.com>
2026-05-25 22:18:23 -0400

    SseModule imported

--- src/send-emails/send-emails.module.ts
+++ src/send-emails/send-emails.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 
 import { SendEmailsController } from '@send-emails/send-emails.controller';
 import { SendEmailsService }    from '@send-emails/send-emails.service';
+import { SseModule }            from '@sse/sse.module';
 
 
 @Module({
     controllers : [ SendEmailsController ],
-    providers   : [ SendEmailsService ]
+    providers   : [ SendEmailsService ],
+    imports     : [ SseModule ]
 })
 export class SendEmailsModule {}
