6a943b87270ffd6300f7208fafae93a3e1b601a6
kevinkeyssx <keyss1700@gmail.com>
2026-05-27 19:01:41 -0400

    SELECT_WORKFLOW added

---
+++ src/workflow/utils/select.ts
@@ -0,0 +1,35 @@
+export const SELECT_WORKFLOW = {
+    id              : true,
+    name            : true,
+    active          : true,
+    students        : true,
+    subject         : true,
+    bcc             : true,
+    cc              : true,
+    frequency       : true,
+    hour            : true,
+    minute          : true,
+    daysOfWeek      : true,
+    dayOfMonth      : true,
+    lastDayOfMonth  : true,
+    occurrences     : true,
+    repeatUntil     : true,
+    neverEnds       : true,
+    templateFileId  : true,
+    filters         : true,
+    template        : {
+        select: {
+            id      : true,
+            content : true,
+        }
+    },
+    templateFile : {
+        select: {
+            id          : true,
+            name        : true,
+            url         : true,
+            coverUrl    : true,
+            type        : true,
+        }
+    }
+}
