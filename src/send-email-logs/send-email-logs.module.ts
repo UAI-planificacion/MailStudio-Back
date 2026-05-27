08c1e916e9dfb7d4cd9dfa83c353e6451b4fbd91
kevinkeyssx <keyss1700@gmail.com>
2026-05-26 21:12:08 -0400

    SseModule importered

--- src/send-email-logs/send-email-logs.module.ts
+++ src/send-email-logs/send-email-logs.module.ts
@@ -3,11 +3,12 @@ import { Module } from '@nestjs/common';
 import { SendEmailLogsService }     from '@send-email-logs/send-email-logs.service';
 import { SendEmailLogsController }  from '@send-email-logs/send-email-logs.controller';
 import { PrismaModule }             from '@prisma/prisma.module';
+import { SseModule }                from '@sse/sse.module';
 
 
-@Module({
-	imports      : [ PrismaModule ],
-	controllers  : [ SendEmailLogsController ],
-	providers    : [ SendEmailLogsService ],
-})
+@Module( {
+	imports     : [ PrismaModule, SseModule ],
+	controllers : [ SendEmailLogsController ],
+	providers   : [ SendEmailLogsService ],
+} )
 export class SendEmailLogsModule {}
